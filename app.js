74fe87409c26bd1448d09d04a0373f5cec7d311c
PaviSabaree <vsabree2016@gmail.com>
2021-06-18 23:05:38 +0530

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+const express=require('express')
+const app=express()
+app.get('/',(req,res)=>{
+    res.send('Home: Hello world')
+})
+app.get('/users',(req,res)=>{
+    res.send('User: Hi!')
+})
+app.listen(3000)
